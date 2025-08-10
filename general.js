bd420c2f9a1a4ec0236763cd13d20f7132870c86
snsksizb <kkonyol178@gmail.com>
2025-08-10 22:09:14 +0700

    Add files via upload

---
+++ general.js
@@ -0,0 +1,41 @@
+export const menuUtama = `
+┏━━〔 🤖 *TermuxBot v3* 〕━━┓
+┃ 📱 *Menu Utama:*
+┃ • .menu - Tampilkan menu
+┃ • .ping - Speed test
+┃ • .runtime - Uptime bot
+┃ • .owner - Kontak owner
+┃ • .sc - Source code
+┣━━〔 *Downloader* 〕━━
+┃ • .play [judul] - YouTube audio
+┃ • .video [judul] - YouTube video
+┃ • .tiktok [url] - TikTok downloader
+┃ • .ig [url] - Instagram downloader
+┣━━〔 *Sticker* 〕━━
+┃ • .sticker - Buat sticker
+┃ • .toimg - Sticker to image
+┃ • .smeme [text1|text2] - Sticker meme
+┣━━〔 *AI & Fun* 〕━━
+┃ • .ai [pertanyaan] - ChatGPT
+┃ • .simi [teks] - SimSimi chat
+┃ • .joke - Random joke
+┗━━━━━━━━━━━━━━━━━━━┛
+`;
+
+export const menuSticker = `
+┏━━〔 *Sticker Menu* 〕━━┓
+┃ • .sticker - Balas gambar/video
+┃ • .toimg - Balas sticker
+┃ • .smeme [atas|bawah] - Meme sticker
+┗━━━━━━━━━━━━━━━━━━━━━┛
+`;
+
+export const menuDownloader = `
+┏━━〔 *Downloader* 〕━━┓
+┃ • .play [judul] - MP3 YouTube
+┃ • .video [judul] - MP4 YouTube
+┃ • .tiktok [url] - No WM
+┃ • .ig [url] - Instagram media
+┃ • .fb [url] - Facebook video
+┗━━━━━━━━━━━━━━━━━━━┛
+`;
